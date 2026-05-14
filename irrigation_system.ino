fac0f31af480a56f5e8b6552682f75a8f3b756f2
Akkala Sambi Reddy <sambireddyakkala2007@gmail.com>
2026-05-14 10:34:23 +0530

    Update irrigation_system.ino

--- irrigation_system.ino
+++ irrigation_system.ino
@@ -3,10 +3,13 @@ int relay = 8;
 
 void setup() {
   pinMode(relay, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
   int moisture = analogRead(sensorPin);
+
+  Serial.println(moisture);
 
   if(moisture > 700) {
     digitalWrite(relay, HIGH);
@@ -14,4 +17,6 @@ void loop() {
   else {
     digitalWrite(relay, LOW);
   }
+
+  delay(1000);
 }
